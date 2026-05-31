7f24333e39d5c5491a1fc0b0a1c7f3576c389af0
William <wla226@sfu.ca>
2026-05-30 22:09:47 -0700

    Lab2

---
+++ lab2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void) {
+  char program[1024];
+
+  while (1) {
+    printf("Enter programs to run.\n");
+    printf("> ");
+    fflush(stdout);
+
+    if (fgets(program, sizeof(program), stdin) == NULL) {
+      break;
+    }
+
+    program[strcspn(program, "\n")] = '\0';
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+      perror("fork");
+      exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+      execl(program, program, (char *)NULL);
+
+      printf("Exec failure\n");
+      exit(EXIT_FAILURE);
+    } else {
+      waitpid(pid, NULL, 0);
+    }
+  }
+
+  return 0;
+}
